2d38e8e55864d8d22e3f3167d2cda30464dadc8e
JDC1492 <Jonathan_Cohen10@yahoo.com>
2021-01-31 17:47:02 -0500

    migration ran for db setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,34 +1,34 @@
 Project.create(
-    title:'',
+    title:'Greenhouse',
     imgUrl:'',
     description:'',
-    gitLink:''
+    gitLink:'https://github.com/JDC1492/cli-project-81720'
 )
 
 Project.create(
-    title:'',
+    title:'Sinatra: VG_Organizer',
     imgUrl:'',
     description:'',
-    gitLink:''
+    gitLink:'https://github.com/JDC1492/vg_organizer-project'
 )
 
 Project.create(
-    title:'',
+    title:'Rails: VG_Organizer',
     imgUrl:'',
     description:'',
-    gitLink:''
+    gitLink:'https://github.com/JDC1492/vg_organizer-app'
 )
 
 Project.create(
-    title:'',
+    title:'Wrap\'d',
     imgUrl:'',
     description:'',
-    gitLink:''
+    gitLink:'https://github.com/JDC1492/wrapd-js_project-front-end'
 )
 
 Project.create(
-    title:'',
+    title:'React: VG_Organizer',
     imgUrl:'',
     description:'',
-    gitLink:''
+    gitLink:'https://github.com/JDC1492/vg_org_frontend'
 )
