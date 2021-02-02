27add04c5aa66e3cade2f2d734e75b963c598459
JDC1492 <Jonathan_Cohen10@yahoo.com>
2021-02-02 10:14:07 -0500

    seed data completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,34 +1,43 @@
 Project.create(
     title:'Greenhouse',
-    imgUrl:'',
-    description:'',
+    imgUrl:'https://pasteboard.co/JMveFkg.png',
+    description:'My CLI project is a "database" of various plant/trees that provides a few facts upon selection. 
+    The API used to collect the data is "https://trefle.io/"',
     gitLink:'https://github.com/JDC1492/cli-project-81720'
 )
 
 Project.create(
-    title:'Sinatra: VG_Organizer',
-    imgUrl:'',
-    description:'',
+    title:'Sinatra:VG_Organizer',
+    imgUrl:'https://pasteboard.co/JMgkf68.png',
+    description:'VG Organizer is a sinatra based web application that gives the user the ability to catalougue their various video games onto one screen. it allows for users to create new entries, update new entries, and even allow them to delete entries that they may no longer have.',
     gitLink:'https://github.com/JDC1492/vg_organizer-project'
 )
 
 Project.create(
-    title:'Rails: VG_Organizer',
-    imgUrl:'',
-    description:'',
+    title:'Rails:VG_Organizer',
+    imgUrl:'https://pasteboard.co/JMvrLut.png',
+    description:'VG Organizer is a Rails based web application that gives the user the ability to save and keep track of their video games. Giving users the functionality of Log-in with an account or Signing up for an account if you dont have one, or even sign-in with an already existing account from a third-party provider. Upon logging in a user can create entries in thier organizer. User also has access to a hall of completion for the entered games that they have completed, and games categorized by console.',
     gitLink:'https://github.com/JDC1492/vg_organizer-app'
 )
 
 Project.create(
     title:'Wrap\'d',
-    imgUrl:'',
-    description:'',
+    imgUrl:'https://pasteboard.co/JMvg9Tc.png',
+    description:'This is a single page application that uses JS, HTML, and Rails(as a backend).
+     The purpose of this application is for the user to create a list of gifts to aid them in thier holiday shopping,
+      identifying the names and items associated with the provided names. Through the use of fetch requests the user is able to create their lists, 
+      view them on the page and delete the entries if necessary. Example Data is provided for example usage in this basic application.',
     gitLink:'https://github.com/JDC1492/wrapd-js_project-front-end'
 )
 
 Project.create(
-    title:'React: VG_Organizer',
-    imgUrl:'',
-    description:'',
+    title:'React:VG_Organizer',
+    imgUrl:'https://pasteboard.co/JMveigQy.png',
+    description:'VG_Organizer - A web-based application.
+     This is a single page application that uses React and Rails(as a backend).
+      The purpose of this application is for the user to create a list.
+       Through the use of fetch requests the user is able to create a catalogue of thier collection,
+        view them on the page and delete the entries if necessary.
+         Example Data is provided for example usage in this basic application.',
     gitLink:'https://github.com/JDC1492/vg_org_frontend'
 )
